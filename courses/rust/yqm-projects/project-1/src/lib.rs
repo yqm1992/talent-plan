c0c747c086ddf6090d521c0939bda58e162b1a4c
yangqiuming <yqm1992ofq@qq.com>
2021-08-19 17:16:41 +0800

    add simple implementation of kvs lib

--- courses/rust/yqm-projects/project-1/src/lib.rs
+++ courses/rust/yqm-projects/project-1/src/lib.rs
@@ -1,1 +1,24 @@
-// just leave it empty for now
+use std::collections::HashMap;
+
+pub struct KvStore {
+    kv_map: HashMap<String, String>,
+}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        KvStore{kv_map: HashMap::new()}
+    }
+    pub fn get(&self, key: String) -> Option<String> {
+        if let Some(val) = self.kv_map.get(&key) {
+            Some(String::from(val))
+        } else {
+            None
+        }
+    }
+    pub fn set(&mut self, key: String, value: String) {
+        self.kv_map.insert(key, value);
+    }
+    pub fn remove(&mut self, key: String) {
+        self.kv_map.remove(&key);
+    }
+}
