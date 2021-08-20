819d3eb24027b0a3976c277f5bfb6461fefd5518
yangqiuming <yqm1992ofq@qq.com>
2021-08-20 09:02:04 +0800

    define data structure and API

--- courses/rust/yqm-projects/project-2/src/lib.rs
+++ courses/rust/yqm-projects/project-2/src/lib.rs
@@ -1,24 +1,71 @@
 use std::collections::HashMap;
+use std::path::PathBuf;
+use std::fs::File;
+use std::io::Write;
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Command {
+    Get(String),
+    Set(String, String),
+    Remove(String),
+}
+
+#[derive(Debug)]
+pub enum KvStoreError {
+    IOError(std::io::Error),
+    JSError(serde_json::Error),
+    ParseError(core::num::ParseIntError),
+    OtherError(String),
+}
+
+pub type Result<T> = std::result::Result<T, KvStoreError>;
+
+/// CommandPos is used to record the file position of a command
+#[derive(Debug)]
+struct CommandPos {
+    /// The log file's index
+    index: u64,
+    /// The offset of value
+    start_pos: u64,
+    /// The length of value
+    len: u64,
+}
+
+/// This represents a log file for KvStore current writing
+struct LogWriteHandle {
+    /// The log file's index
+    index: u64,
+    /// The log file's write handle
+    file: File,
+    /// The position to write next time
+    next_pos: u64,
+}
 
 pub struct KvStore {
-    kv_map: HashMap<String, String>,
+    /// The root directory
+    root_dir_path_buf: PathBuf,
+    /// The in-memory index from key to log pointer
+    kv_map: HashMap<String, CommandPos>,
+    /// The handle of current writing log file
+    log_write_handle: LogWriteHandle,
 }
 
 impl KvStore {
-    pub fn new() -> KvStore {
-        KvStore{kv_map: HashMap::new()}
+    /// Open the database
+    pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
+        Err(KvStoreError::OtherError("no implementation".to_string()))
     }
-    pub fn get(&self, key: String) -> Option<String> {
-        if let Some(val) = self.kv_map.get(&key) {
-            Some(String::from(val))
-        } else {
-            None
-        }
+    /// Get the value of key from KvStore
+    pub fn get(&mut self, key: String) -> Result<Option<String>> {
+        Err(KvStoreError::OtherError("no implementation".to_string()))
     }
-    pub fn set(&mut self, key: String, value: String) {
-        self.kv_map.insert(key, value);
+    /// Set value for key in KvStore
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
+        Err(KvStoreError::OtherError("no implementation".to_string()))
     }
-    pub fn remove(&mut self, key: String) {
-        self.kv_map.remove(&key);
+    /// Remove a key from KvStore
+    pub fn remove(&mut self, key: String) -> Result<()> {
+        Err(KvStoreError::OtherError("no implementation".to_string()))
     }
 }
